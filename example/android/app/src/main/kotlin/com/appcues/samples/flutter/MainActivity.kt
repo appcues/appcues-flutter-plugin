77c8c221a0f3e77cd043207cb9128f90eebac7bf
James Ellis <james@appcues.com>
2023-07-21 15:20:21 -0400

    ♻️ Replace usage of uni_links package with our own deep link handling

--- example/android/app/src/main/kotlin/com/appcues/samples/flutter/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/appcues/samples/flutter/MainActivity.kt
@@ -1,6 +1,67 @@
 package com.appcues.samples.flutter
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterFragmentActivity() {
+    companion object {
+        const val CHANNEL = "com.appcues.samples.flutter/channel"
+        const val EVENTS = "com.appcues.samples.flutter/events"
+    }
+
+    private var startString: String? = null
+    private var linksReceiver: BroadcastReceiver? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "initialLink") {
+                if (startString != null) {
+                    result.success(startString)
+                }
+            }
+        }
+
+        EventChannel(flutterEngine.dartExecutor, EVENTS).setStreamHandler(
+            object : EventChannel.StreamHandler {
+                override fun onListen(args: Any?, events: EventChannel.EventSink) {
+                    linksReceiver = createChangeReceiver(events)
+                }
+
+                override fun onCancel(args: Any?) {
+                    linksReceiver = null
+                }
+            }
+        )
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startString = intent.data?.toString()
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        if (intent.action === Intent.ACTION_VIEW) {
+            linksReceiver?.onReceive(this.applicationContext, intent)
+        }
+    }
+
+    fun createChangeReceiver(events: EventChannel.EventSink): BroadcastReceiver? {
+        return object : BroadcastReceiver() {
+            // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
+            override fun onReceive(context: Context, intent: Intent) {
+                val dataString = intent.dataString ?:
+                events.error("UNAVAILABLE", "Link unavailable", null)
+                events.success(dataString)
+            }
+        }
+    }
 }
