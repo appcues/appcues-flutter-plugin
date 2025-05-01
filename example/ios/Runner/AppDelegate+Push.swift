695a67a0af86d20c59a2bef0a5d862e0760c6253
Matt Hayashida <matt.hayashida@appcues.com>
2025-05-01 10:51:46 -0400

    🎬 Add manual iOS push configuration example

---
+++ example/ios/Runner/AppDelegate+Push.swift
@@ -0,0 +1,55 @@
+//
+//  AppDelegate+Push.swift
+//  Runner
+//
+//  Created by Matt on 2025-05-01.
+//
+
+// Disabled in favor of automatic configuration
+
+/*
+import Foundation
+import UserNotifications
+import appcues_flutter
+
+extension AppDelegate {
+    /// Call from `UIApplicationDelegate.application(_:didFinishLaunchingWithOptions:)`
+    func setupPush(application: UIApplication) {
+        // 1: Register to get a device token
+        application.registerForRemoteNotifications()
+
+        UNUserNotificationCenter.current().delegate = self
+    }
+
+    // 2: Pass device token to Appcues
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+      SwiftAppcuesFlutterPlugin.setPushToken(deviceToken)
+    }
+
+    // 3: Pass the user's response to a delivered notification to Appcues
+    override func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
+        if SwiftAppcuesFlutterPlugin.didReceiveNotification(response: response, completionHandler: completionHandler) {
+            return
+        }
+
+        super.userNotificationCenter(center, didReceive: response, withCompletionHandler: completionHandler)
+    }
+
+    // 4: Configure handling for notifications that arrive while the app is in the foreground
+    override func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.banner, .list])
+        } else {
+            completionHandler(.alert)
+        }
+    }
+}
+*/
